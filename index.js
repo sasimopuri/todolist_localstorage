ecd9db34bfdfceac36ee6d1e6945572afad283d5
sasimopuri <115964652+sasimopuri@users.noreply.github.com>
2022-11-04 20:30:00 +0530

    get name

---
+++ index.js
@@ -0,0 +1,19 @@
+window.onload=()=>{
+    if()
+}
+
+
+
+// let getinnername=document.querySelector(".input")
+// function updatename(){
+//     getname=document.querySelector("#name")
+//     if(getname.value.length==0)
+//     {
+//         alert("Enter name")
+//     }
+//     else{
+//         getinnername.innerHTML=`<span class="greeting">
+//         Hey!! <span class="nameis">${getname.value}</span>
+//     </span>`
+//     }
+// }   
