8b849320a0d3bca0cc0b6c9b24e6f5917db7bf35
sasimopuri <115964652+sasimopuri@users.noreply.github.com>
2022-11-04 22:22:59 +0530

    get name

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 window.onload=()=>{
-    if()
+    let getname= localStorage.getItem('username') || '';
+    if(getname.length!=0)
+    {
+        changenamedom()
+    }
+    
 }
 
+let getinnername=document.querySelector(".input")
+function addname(){
+    getname=document.querySelector("#name")
+    if(getname.value.length==0)
+    {
+        alert("Enter name")
+    }
+    else{
+        localStorage.setItem('username',getname.value)
+        getinnername.innerHTML=`<span class="greeting">
+        Hey!! <span class="nameis">${getname.value}</span>
+    </span>`
+    }
+}   
 
-
-// let getinnername=document.querySelector(".input")
-// function updatename(){
-//     getname=document.querySelector("#name")
-//     if(getname.value.length==0)
-//     {
-//         alert("Enter name")
-//     }
-//     else{
-//         getinnername.innerHTML=`<span class="greeting">
-//         Hey!! <span class="nameis">${getname.value}</span>
-//     </span>`
-//     }
-// }   
+function changenamedom()
+{
+    let getnamels=localStorage.getItem("username")
+    getinnername.innerHTML=`<span class="greeting">
+        Hey!! <span class="nameis">${getnamels.toUpperCase()}</span>
+    </span>`
+}
